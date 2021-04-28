063d50bc895c04aedeb43c4151a0e23da38dd1a9
Arnau <arnaucinca1998@gmail.com>
2021-04-28 16:39:13 +0200

    organize + Object

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void pick()
+    {
+
+    }
+    public void cut()
+    {
+
+    }
+    public void cook_oven()
+    {
+
+    }
+
+    public void cook_stove()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tomato.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tomato : Object
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void pick()
+    {
+
+    }
+    void cut()
+    {
+
+    }
+    void cook_oven()
+    {
+
+    }
+
+    void cook_stove()
+    {
+
+    }
+}
+
