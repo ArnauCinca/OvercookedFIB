8152bfcb24b53ee38406449e7b20e7ccdf59ba13
Arnau <arnaucinca1998@gmail.com>
2021-05-28 22:12:47 +0200

    Stove sound

--- Assets/Scripts/Furniture/Stove.cs
+++ Assets/Scripts/Furniture/Stove.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Stove : Furniture
 {
+    private AudioSource source;
+    private bool r = false;
+
     // Start is called before the first frame update
     public void Start()
     {
+        source = GetComponent<AudioSource>();
+
         fire = null;
         if (spawnObject != null)
             o = Instantiate(spawnObject, new Vector3(transform.position.x, transform.position.y + 2.0f, transform.position.z), Quaternion.identity);
@@ -17,6 +22,17 @@ public class Stove : Furniture
         base.Update();
         gameObject.transform.GetChild(0).gameObject.SetActive(!isWorking);
         gameObject.transform.GetChild(1).gameObject.SetActive(isWorking);
+        if (isWorking && !r)
+        {
+            Debug.Log("Play");
+            source.Play();
+            r = true;
+        }
+        else if (!isWorking && r)
+        {
+            source.Stop();
+            r = false;
+        }
     }
     public override bool action(GameObject go)
     {
